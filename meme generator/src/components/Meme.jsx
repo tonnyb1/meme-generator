fd94965d9c84a4e8567c940d5ffd5e6496b76b27
Tonnyb <tonnyb@joingladys.com>
2023-02-28 12:32:20 +0300

    using state to change value of variable

--- meme generator/src/components/Meme.jsx
+++ meme generator/src/components/Meme.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import memesData from '../assets/data/memesData'
 
 export default function Meme() {
+
+    const [imageUrl, setImageUrl] = React.useState("")
+    const memesArray = memesData?.data?.memes
+    const randomNum = Math.floor(Math.random() * memesArray.length)
+    const imageUrli = memesData.data.memes[randomNum].url
+    console.log("mimin", randomNum)
+
+    console.log("urlis",imageUrli)
+    
+    function getImage () {
+        setImageUrl(imageUrli)
+    }
+    
     return (
         <main>
             <form className="form">
-            <input type="text" className="memeinput" placeholder="Top text"/>
-            <input type="text" className="memeinput" placeholder="Bottom text"/>
-            <button className="memebutton">Get a new meme image  🖼</button>
-        </form>
+                <input type="text" className="memeinput" placeholder="Top text"/>
+                <input type="text" className="memeinput" placeholder="Bottom text"/>
+                <button className="memebutton" onClick={getImage}>Get a new meme image  🖼</button>
+            </form>
+            <img src= {imageUrli} alt="meme" className="memeimage"/>
         </main>
         
     )
